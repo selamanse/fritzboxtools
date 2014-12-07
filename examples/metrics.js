f68a6a147e7fb54911466a0e16b71473dbbe76c9
Selamanse <ferdi@gueran.de>
2014-12-07 22:13:20 +0100

    added getdata method for myfritz section support. tested with FRITZ\!OS 06.20 and user with restricted rights for myfritz section only

--- examples/metrics.js
+++ examples/metrics.js
@@ -1,14 +1,13 @@
 var fritzboxtools = require('../lib/fritzboxtools');
 var argv = require('optimist')
-			.usage('Usage: $0 --fboxuri [string] --user [string] --pass [string] --deviceID [num] --state [0|1]')
-    		.demand(['user', 'pass', 'deviceID'])
+			.usage('Usage: $0 --fboxuri [string] --user [string] --pass [string]')
+    		.demand(['user', 'pass'])
     		.default('fboxuri', 'fritz.box')
-    		.default('state', 0)
     		.argv;
 
 
 var fbox = new fritzboxtools(argv.user, argv.pass, argv.fboxuri);
 
 fbox.login(function(){
-	fbox.SwitchOnOff(argv.deviceID, argv.state);
+	fbox.getData();
 });
